f5b35cdfa779d3f273aa8cd3842530118030687e
Cida Luna <cidalunna@hotmail.com>
2025-02-23 23:55:34 -0300

    Add error interceptor class

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ErrorInterceptor } from './core/error.interceptor';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideAnimations(),
+    BrowserModule,
+    MatTableModule,
+    MatSnackBarModule,
     provideHttpClient(),
-    MatTableModule
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
   ]
 };
---
+++ src/app/core/error.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error) => {
+        console.error('Erro no request', error);
+        throw error;  // Re-throwing to propagate the error
+      })
+    );
+  }
+}
