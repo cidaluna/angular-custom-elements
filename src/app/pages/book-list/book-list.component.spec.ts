a092436399e34defb559ffc78c0ca4e29cbf5c9e
Cida Luna <cidalunna@hotmail.com>
2025-03-11 21:42:28 -0300

    Fix imports on spec ts

--- src/app/pages/book-list/book-list.component.spec.ts
+++ src/app/pages/book-list/book-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
@@ -8,10 +10,10 @@ describe('BookListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BookListComponent]
+      imports: [BookListComponent, HttpClientTestingModule, NoopAnimationsModule]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
