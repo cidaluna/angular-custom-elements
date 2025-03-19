0f39b4cb3f5e17c0793c2b13055aa9528c77be0b
Cida Luna <cidalunna@hotmail.com>
2025-03-18 23:40:34 -0300

    Add mat paginator module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { errorInterceptor } from './core/error.interceptor';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {MatSelectModule} from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,5 +26,6 @@ export const appConfig: ApplicationConfig = {
     MatSelectModule,
     MatDialogModule,
     MatIconModule,
+    MatPaginatorModule,
   ]
 };
