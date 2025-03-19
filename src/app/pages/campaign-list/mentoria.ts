d9f0a7e5cdbc384e3b0e3ea0191bd1d6d06215f6
Cida Luna <cidalunna@hotmail.com>
2025-03-18 23:40:06 -0300

    Remove file

--- src/app/pages/campaign-list/mentoria.ts
+++
@@ -1,7 +0,0 @@
-// Buscar dados do JSON Server utilizando método GET
-
-
-// Trabalhar no campo SELECT booleano, mas na tela exibir Sim ou Não
-
-
-// Trabalhar em todos os filtros da tela, tabela, paginação, modal, etc
